d3a0ac139af1d7fe2b96d3475bcaf2ec52cf7c0c
sathian <sathian.k@customeranalytics.com>
2020-01-31 11:27:54 +0530

    sdfsf

--- src/android/Test1.java
+++ src/android/Test1.java
@@ -1,4 +1,4 @@
-package cordova.plugin.Test1;
+package com.cordova.plugin.Test1;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
@@ -6,12 +6,14 @@ import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import com.example.*;
+import com.example.helloworldlib.HelloWorld;
 
 /**
  * This class echoes a string called from JavaScript.
  */
 public class Test1 extends CordovaPlugin {
+
+    private static HelloWorld hw = new HelloWorld();
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
@@ -25,7 +27,7 @@ public class Test1 extends CordovaPlugin {
 
     private void coolMethod(String message, CallbackContext callbackContext) {
         if (message != null && message.length() > 0) {
-            callbackContext.success(message);
+            callbackContext.success(message + ":" + hw.getEnglish());
         } else {
             callbackContext.error("Expected one non-empty string argument.");
         }
